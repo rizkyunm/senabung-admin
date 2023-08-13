3d46d0f393db3dec9fd5344310f6a995063ee65b
rizkyachmadsyah <achmad.rizky@misteraladin.com>
2023-08-14 02:24:15 +0700

    add start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './.output/server/index.mjs',
+      args: 'start',
     },
   ],
 }
