f3b91d65d1f2053cff3547cd7c85d97ccb2e2539
rizkyachmadsyah <achmad.rizky@misteraladin.com>
2023-08-14 03:29:09 +0700

    fix: error build

---
+++ .output/public/_nuxt/index.14cdcfa9.js
@@ -0,0 +1,41 @@
+import{f as l,u as n,r as d,o as c,c as m,a as s,b as t,w as i,g as e,v as a,d as o,t as r}from"./entry.76d01983.js";const p={class:""},f=s("h1",{class:"text-3xl font-bold"},"Offcanvas",-1),v=s("hr",{class:"my-2 border dark:border-gray-700"},null,-1),q={class:"grid grid-cols-12 gap-2"},b={class:"col-span-12"},g={class:"grid md:flex gap-2"},h={class:"p-4 overflow-auto"},_={class:"p-4 overflow-auto"},x={class:"p-4 overflow-auto"},w={class:"p-4 overflow-auto"},T={class:"p-4 overflow-auto"},O={class:"p-4 overflow-auto"},E=l({__name:"index",setup(B){n({title:"Offcanvas"});const u=d(`
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi pariatur maxime, obcaecati doloremque ea
+cupiditate debitis provident sed corporis eos culpa vel voluptatum dolore odit soluta cum aut fugiat
+esse. Necessitatibus, vero rem. Itaque laudantium autem quos fugiat delectus molestias iusto velit
+sapiente. Iure porro voluptate dignissimos? Quis, exercitationem corporis. Dolorem nulla veritatis ut
+amet dicta repellendus sed, nobis labore ipsum? Corporis nostrum fugit nesciunt cupiditate libero iusto
+expedita illo magni cumque odit. Labore, quibusdam! Officia maxime recusandae exercitationem mollitia
+voluptatibus ex id, nobis similique reprehenderit distinctio? Tempora magni sequi fugiat minus obcaecati
+pariatur, nemo animi cumque, eaque aliquid suscipit quo repellendus neque aliquam. Quidem atque laborum
+officiis pariatur dolores ea consequatur ipsa! Repudiandae rerum quibusdam fugit. Rerum officiis qui
+obcaecati unde voluptatibus? Eaque praesentium architecto hic voluptatibus nihil ullam, voluptates totam
+incidunt officia alias laboriosam voluptate qui molestiae neque adipisci reiciendis eos corporis ut,
+sequi accusantium. Nostrum illo temporibus quo, nobis reprehenderit repellat ea magni? Est ad sed,
+quidem eius excepturi dignissimos quasi nemo velit. Excepturi, nobis. Officiis vitae veritatis
+consequuntur nam. Quam consequatur laborum at. Vero, mollitia veniam! Corporis consectetur dolorem
+accusantium velit quasi beatae molestias odio tenetur quod iusto, eaque id dolore culpa a eveniet minima
+eius ipsam? Saepe consequuntur accusantium laudantium asperiores delectus doloremque tenetur,
+perferendis dicta itaque. Explicabo ratione, nemo voluptatum fugit eligendi, obcaecati consectetur culpa
+rem repellendus quos, odio modi officia illo iste dolorem similique esse odit voluptatem velit error
+commodi cupiditate facilis! Ratione amet voluptatibus ad dignissimos dicta ducimus unde voluptate
+voluptatum id, placeat nesciunt atque similique provident perspiciatis voluptatem quaerat officiis eaque
+molestiae in neque tempore. Aliquid aperiam, sit unde nostrum provident nesciunt maxime! Id dignissimos
+laboriosam distinctio vitae, blanditiis alias impedit natus quasi, rem libero at ea temporibus eaque,
+nihil consectetur ut labore! Voluptate atque, fuga laborum nisi blanditiis molestias commodi aspernatur
+pariatur sequi aliquid expedita culpa fugit tenetur distinctio libero, beatae odit vel. Accusantium
+soluta expedita eius? Tenetur omnis aperiam quo officiis quas, porro, incidunt soluta deleniti at rem
+odit fugit ut tempora rerum quod dolorum dolores sunt eius inventore voluptatem nulla debitis ab. Natus
+molestiae dicta iure nisi odit, aliquam non vel, soluta ipsa autem quia ullam error. Possimus veniam
+consequuntur enim deleniti nulla, impedit maxime cupiditate ut modi nihil quod, iusto aut excepturi
+reprehenderit nobis non ipsam tempore. Commodi tempore qui nemo soluta consequuntur sequi accusantium
+ipsam fugiat fugit aspernatur. Cumque debitis, sapiente odit enim aperiam soluta sequi at voluptates?
+Incidunt sequi asperiores quas atque laudantium ad perspiciatis? Perferendis quae cum molestiae
+explicabo, cumque repudiandae, voluptas ut aliquam hic nobis enim molestias perspiciatis. Commodi,
+tenetur quaerat similique sequi ea eius, aut nostrum distinctio et dignissimos fuga voluptatibus dolorem
+reiciendis quo qui. Possimus dolor ullam culpa unde doloremque consequuntur ea vero, saepe molestiae
+laboriosam? Corporis veritatis odit accusamus hic quod laboriosam perferendis temporibus, repellendus
+consequatur culpa, eaque ab ipsa iste facilis laudantium! Non aut dolorem obcaecati eveniet odio, illo
+dolores ipsum sint sequi porro corporis odit suscipit nulla vitae! Rem, incidunt laboriosam consequuntur
+magni omnis illo doloremque sed? Ipsum optio dolore delectus distinctio voluptatibus. Sed expedita
+officiis debitis fuga repellendus? Quam!
+`);return(R,C)=>(c(),m("div",p,[f,v,s("div",q,[s("div",b,[s("div",g,[t(e(a.TwOffcanvas),{position:"left"},{trigger:i(()=>[t(e(a.TwButton),null,{default:i(()=>[o(" Left Offcanvas ")]),_:1})]),headerTitle:i(()=>[o(" Left Offcanvas ")]),default:i(()=>[s("div",h,r(e(u)),1)]),_:1}),t(e(a.TwOffcanvas),{position:"right"},{trigger:i(()=>[t(e(a.TwButton),null,{default:i(()=>[o(" Right Offcanvas ")]),_:1})]),headerTitle:i(()=>[o(" Right Offcanvas ")]),default:i(()=>[s("div",_,r(e(u)),1)]),_:1}),t(e(a.TwOffcanvas),{position:"right",width:"800px"},{trigger:i(()=>[t(e(a.TwButton),null,{default:i(()=>[o(" Right Offcanvas with width 800px ")]),_:1})]),headerTitle:i(()=>[o(" Right Offcanvas ")]),default:i(()=>[s("div",x,r(e(u)),1)]),_:1}),t(e(a.TwOffcanvas),{position:"left",width:"600px"},{trigger:i(()=>[t(e(a.TwButton),null,{default:i(()=>[o(" Left Offcanvas with width 600px ")]),_:1})]),headerTitle:i(()=>[o(" Right Offcanvas ")]),default:i(()=>[s("div",w,r(e(u)),1)]),_:1}),t(e(a.TwOffcanvas),{position:"top"},{trigger:i(()=>[t(e(a.TwButton),null,{default:i(()=>[o(" Top Offcanvas ")]),_:1})]),headerTitle:i(()=>[o(" Top Offcanvas ")]),default:i(()=>[s("div",T,r(e(u)),1)]),_:1}),t(e(a.TwOffcanvas),{position:"bottom"},{trigger:i(()=>[t(e(a.TwButton),null,{default:i(()=>[o(" Bottom Offcanvas ")]),_:1})]),headerTitle:i(()=>[o(" Bottom Offcanvas ")]),default:i(()=>[s("div",O,r(e(u)),1)]),_:1})])])])]))}});export{E as default};
